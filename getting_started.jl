64d1eb5ba36918910cb75fe0966ed2e4cf93febf
Dale Black <djblack@uci.edu>
2023-10-05 11:46:35 -0700

    init

---
+++ getting_started.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+#> [frontmatter]
+#> title = "Getting Started"
+#> category = "Tutorials"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4b5d4dfc-63af-11ee-096f-3f68748d168f
+md"""
+# Getting Started
+"""
+
+# ╔═╡ Cell order:
+# ╟─4b5d4dfc-63af-11ee-096f-3f68748d168f
--- index.jl
+++ index.jl
@@ -1,6 +1,10 @@
 ### A Pluto.jl notebook ###
 # v0.19.26
 
+#> [frontmatter]
+#> title = "GlassDocs"
+#> sidebar = "false"
+
 using Markdown
 using InteractiveUtils
 
