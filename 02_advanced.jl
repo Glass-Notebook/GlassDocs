8afacdc456c14664c2eb29d043c3672e1ac8c729
Dale Black <djblack@uci.edu>
2023-10-11 16:16:34 -0700

    update

---
+++ 02_advanced.jl
@@ -0,0 +1,35 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 463236e3-f8e0-4748-bbfc-4a4331c61ea6
+# ╠═╡ show_logs = false
+begin
+	using Pkg
+	Pkg.activate(".")
+	Pkg.instantiate()
+
+	using PlutoUI
+	using HTMLStrings
+end
+
+# ╔═╡ f4914413-531e-408f-85bc-87448e9c2987
+TableOfContents()
+
+# ╔═╡ 4b2f8bda-a3c1-4ccd-92e8-fb19badef84f
+md"""
+## Index (Homepage)
+"""
+
+# ╔═╡ d3942b60-f85a-439d-98f4-8f4da24b830a
+md"""
+## Glass Export File
+"""
+
+# ╔═╡ Cell order:
+# ╠═463236e3-f8e0-4748-bbfc-4a4331c61ea6
+# ╠═f4914413-531e-408f-85bc-87448e9c2987
+# ╟─4b2f8bda-a3c1-4ccd-92e8-fb19badef84f
+# ╟─d3942b60-f85a-439d-98f4-8f4da24b830a
