cd8983ccbe8a4636520ff685f870760131361f79
Dale Black <djblack@uci.edu>
2023-10-11 16:13:36 -0700

    add getting started docs

--- 01_getting_started.jl
+++ 01_getting_started.jl
@@ -16,11 +16,14 @@ begin
 	Pkg.instantiate()
 
 	using PlutoUI
-	using HTMLStrings
+	using HTMLStrings: script, to_html, img
 end
 
 # ╔═╡ 7d73a5fc-b9e9-4d04-90ec-e57f19eddf38
 TableOfContents()
+
+# ╔═╡ 93828e96-dda7-42a8-9524-c1fdc098a321
+to_html(script(:src => "https://cdn.tailwindcss.com"))
 
 # ╔═╡ 4b5d4dfc-63af-11ee-096f-3f68748d168f
 md"""
@@ -32,98 +35,117 @@ md"""
 # ╔═╡ ceb890af-3537-4286-8734-5309b0d11648
 md"""
 ## 1. Sign In
-
-a. Type in `https://glassnotebook.io/` and click "Get Started" in the upper right hand corner.
-
-b. Click sign in with GitHub, then authorize Glass Notebook to your GitHub account
 """
 
-# ╔═╡ 331bcbce-52b7-4f1f-b6a0-94a5e90eeb87
+# ╔═╡ f830e89e-4c8d-4477-a827-ff894bd9f0e4
+md"""
+Type in `https://glassnotebook.io/` and click "Get Started" in the upper right hand corner.
+"""
+
+# ╔═╡ 8dc6060f-6112-49f1-9856-c3e971cc5a7f
 to_html(
-	divv(
-		head(
-			script(:src => "https://cdn.tailwindcss.com")
-		),
-		divv(:class => "flex flex-wrap justify-center items-start gap-4",
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/homepage.png?raw=true"
-			),
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/authgithub.png?raw=true"
-			),
-		)
-	)
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/homepage.png?raw=true"
+	),
+)
+
+# ╔═╡ ef9912d7-4336-4f5f-bb59-a4e46bedf77f
+md"""
+Click sign in with GitHub, then authorize Glass Notebook to your GitHub account
+"""
+
+# ╔═╡ ef54e0f4-b8d9-4782-b267-80f90d40d97b
+to_html(
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/authgithub.png?raw=true"
+	),
 )
 
 # ╔═╡ a1284a2c-977e-4df5-aa0e-ba5651b62fe6
 md"""
 ## 2. Link Repository
-
-a. Add repository by pasting the GitHub URL or directly through GitHub
-
-b. Choose `private` or `pubic` option for linked repositories
-
-c. Choose whether or not to make notebooks interactive by toggling the cloud near the side of the individual notebook
 """
 
-# ╔═╡ 4543f920-bfc2-42cd-a689-66743ded5681
+# ╔═╡ 3ee24a14-4343-4c86-b7eb-3f82b2368355
+md"""
+Add repository by pasting the GitHub URL or directly through GitHub.
+"""
+
+# ╔═╡ 4f7fe12a-9497-4e4b-85d0-c0505f4a1c09
 to_html(
-	divv(
-		head(
-			script(:src => "https://cdn.tailwindcss.com")
-		),
-		divv(:class => "flex flex-wrap justify-center items-start gap-4",
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/addnotebook.png?raw=true"
-			),
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/toggleprivate.png?raw=true"
-			),
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/toggleinteractive.png?raw=true"
-			),
-		)
-	)
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/addnotebook.png?raw=true"
+	),
+)
+
+# ╔═╡ e01f0a53-228f-4359-b8f3-1b5b2f09c412
+md"""
+Choose `private` or `pubic` option for linked repositories
+"""
+
+# ╔═╡ 67737d20-5f67-4ac3-83c8-66bd8c636c56
+to_html(
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/toggleprivate.png?raw=true"
+	),
+)
+
+# ╔═╡ 381bb5c1-de79-47d1-bc59-2f663e6e7cf6
+md"""
+Choose whether or not to make notebooks interactive by toggling the cloud near the side of the individual notebook
+"""
+
+# ╔═╡ 42c366a9-cdb1-44e2-a1bc-80345ff6d0cd
+to_html(
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/toggleinteractive.png?raw=true"
+	),
 )
 
 # ╔═╡ f2817881-2427-41b5-925c-86a88fcae4d7
 md"""
 ## 3. Share Notebook(s)
+"""
 
-a. Copy and paste the notebook URL to share with others
+# ╔═╡ 02868e2f-f6a7-4e53-ae14-93cf2b932b88
+md"""
+Copy and paste the notebook URL to share with others
 
 *Note*: we have a simple markdown badge for any documentation built with Glass
 
 [![Glass Notebook](https://img.shields.io/badge/Docs-Glass%20Notebook-aquamarine.svg)](https://glassnotebook.io/r/zn0Xr-A1cYegYHSDJzSyO/docs/index.jl)
 """
 
-# ╔═╡ 550edfe8-dfa2-472d-9e40-d04355177243
+# ╔═╡ a7e8bfae-fc0b-41c2-9576-bcfdc7c8b03c
 to_html(
-	divv(
-		head(
-			script(:src => "https://cdn.tailwindcss.com")
-		),
-		divv(:class => "flex flex-wrap justify-center items-start gap-4",
-			img(
-				:class => "w-1/2 rounded-md",
-				:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/sharenotebook.png?raw=true"
-			),
-		)
-	)
+	img(
+		:class => "w-full rounded-md pb-2",
+		:src => "https://github.com/Dale-Black/GlassDocs/blob/master/images/sharenotebook.png?raw=true"
+	),
 )
 
 # ╔═╡ Cell order:
 # ╠═f8b4d8a6-e7c3-4e93-96b6-8ae988fb79f6
 # ╠═7d73a5fc-b9e9-4d04-90ec-e57f19eddf38
+# ╠═93828e96-dda7-42a8-9524-c1fdc098a321
 # ╟─4b5d4dfc-63af-11ee-096f-3f68748d168f
 # ╟─ceb890af-3537-4286-8734-5309b0d11648
-# ╠═331bcbce-52b7-4f1f-b6a0-94a5e90eeb87
+# ╟─f830e89e-4c8d-4477-a827-ff894bd9f0e4
+# ╟─8dc6060f-6112-49f1-9856-c3e971cc5a7f
+# ╟─ef9912d7-4336-4f5f-bb59-a4e46bedf77f
+# ╟─ef54e0f4-b8d9-4782-b267-80f90d40d97b
 # ╟─a1284a2c-977e-4df5-aa0e-ba5651b62fe6
-# ╠═4543f920-bfc2-42cd-a689-66743ded5681
+# ╟─3ee24a14-4343-4c86-b7eb-3f82b2368355
+# ╟─4f7fe12a-9497-4e4b-85d0-c0505f4a1c09
+# ╟─e01f0a53-228f-4359-b8f3-1b5b2f09c412
+# ╟─67737d20-5f67-4ac3-83c8-66bd8c636c56
+# ╟─381bb5c1-de79-47d1-bc59-2f663e6e7cf6
+# ╟─42c366a9-cdb1-44e2-a1bc-80345ff6d0cd
 # ╟─f2817881-2427-41b5-925c-86a88fcae4d7
-# ╠═550edfe8-dfa2-472d-9e40-d04355177243
+# ╟─02868e2f-f6a7-4e53-ae14-93cf2b932b88
+# ╟─a7e8bfae-fc0b-41c2-9576-bcfdc7c8b03c
