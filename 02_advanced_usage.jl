55d70eafa15557258bfde6354248f0060e5eb423
Dale Black <djblack@uci.edu>
2024-02-17 14:15:05 -0800

    update docs

--- 02_advanced_usage.jl
+++ 02_advanced_usage.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.36
+# v0.19.38
 
 #> [frontmatter]
 #> title = "Advanced Usage"
@@ -43,9 +43,29 @@ Repository.jl/test/expensive_notebook.jl
 ```
 """
 
+# ╔═╡ 7b8cc940-fbba-42ed-b5b7-30abca9feb99
+md"""
+## Notebook Export Options
+
+Glass Notebook provides different export options to accommodate various user needs, ranging from simple viewing to full interactivity with real-time data handling capabilities.
+
+| Notebook Type | Interactivity Level | Data Injection Capability | Relative Cost |
+|---------------|---------------------|---------------------------|---------------|
+| Static Notebooks| None | No | Lowest |
+| Precomputed Notebooks| Interactive UIs | No | Medium |
+| Fully Interactive | Full | Yes (e.g., data uploads)| Highest |
+"""
+
+# ╔═╡ 1de7c073-36d0-4f29-80d8-5c56d0cbf23f
+md"""
+## Links
+
+Links work inside markdown like normal and relative links (e.g. `[link to other notebook](/docs/other_notebook.jl)`) will be automatically parsed and linked properly in Glass
+"""
+
 # ╔═╡ 43c3c5c8-95bd-471e-b091-ed5f794ea0e2
 md"""
-## Sidebar
+# Level 1. Sidebar
 
 When exporting, Glass will automatically create a sidebar for nice navigation. This sidebar will, by default, be the name of the files and their relative paths. To enhance the user experience Glass also allows for customization via the built-in `edit frontmatter` button in Pluto. All that is required is to change the title of the notebook via `edit frontmatter`, and the sidebar will automatically adjust during the next export. 
 """
@@ -92,39 +112,19 @@ index_url = "https://github.com/Dale-Black/GlassDocs/blob/master/assets/index.jp
 # ╔═╡ 69201461-d1ee-4e4d-b17e-459508748967
 Resource(index_url)
 
-# ╔═╡ 1de7c073-36d0-4f29-80d8-5c56d0cbf23f
-md"""
-## Links
-
-Links work inside markdown like normal and relative links (e.g. `[link to other notebook](/docs/other_notebook.jl)`) will be automatically parsed and linked properly in Glass
-"""
-
-# ╔═╡ 7b8cc940-fbba-42ed-b5b7-30abca9feb99
-md"""
-## Notebook Export Options
-
-Glass Notebook provides different export options to accommodate various user needs, ranging from simple viewing to full interactivity with real-time data handling capabilities.
-
-| Notebook Type | Interactivity Level | Data Injection Capability | Relative Cost |
-|---------------|---------------------|---------------------------|---------------|
-| Static Notebooks| None | No | Lowest |
-| Precomputed Notebooks| Interactive UIs | No | Medium |
-| Fully Interactive | Full | Yes (e.g., data uploads)| Highest |
-"""
-
 # ╔═╡ e3b98336-7c90-4a5c-a09c-b3d6297506e3
 md"""
-## Meta-Docs
+# Level 2. Meta-Docs
 
 The `structure.json` file in Glass Notebook is a crucial tool for crafting a custom sidebar for your documentation. It's especially valuable when your documentation integrates multiple repositories or has complex structures.
 
-### Git Submodules and `structure.json` in Glass Notebook
+## Git Submodules and `structure.json` in Glass Notebook
 
 - **Git Submodules**: These are used to incorporate separate repositories as part of your project while keeping their individual commit histories. In Glass Notebook, submodules enable you to include content from various repositories into a single comprehensive documentation system.
 
 - **`structure.json`**: This is a configuration file used to explicitly define the structure of the sidebar. It allows for more detailed organization than the default automatic indexing, enabling you to create a customized layout for your documentation.
 
-### Example `structure.json`
+## Example `structure.json`
 
 Below is an example of a `structure.json` file, which outlines a sidebar with various components including nested folders and references to git submodules:
 
@@ -212,7 +212,7 @@ The `structure.json` file shapes the sidebar for GlassDocs.jl documentation. The
 
 These collapsible folders help to manage space and maintain an organized layout, especially beneficial for extensive documentation. Users can expand these folders to access specific content.
 
-### Implementing `structure.json`
+## Implementing `structure.json`
 
 To utilize this feature:
 
@@ -225,12 +225,21 @@ This custom sidebar structure enhances the navigation experience for users explo
 """
 
 
+# ╔═╡ 039a9c77-fed4-45b5-a8cc-cec13224ddb2
+md"""
+# Level 3. Nested Meta-Docs (`navbar.json`)
+
+[Coming Soon] - Connect multiple Meta-Docs together via a `navbar.json` file
+"""
+
 # ╔═╡ Cell order:
 # ╠═46581809-1673-45c7-8cc1-40bf12b2a8d7
 # ╠═463236e3-f8e0-4748-bbfc-4a4331c61ea6
 # ╠═f4914413-531e-408f-85bc-87448e9c2987
 # ╟─9db1c05e-c47c-42a5-afea-041344a12ad2
 # ╟─d3942b60-f85a-439d-98f4-8f4da24b830a
+# ╟─7b8cc940-fbba-42ed-b5b7-30abca9feb99
+# ╟─1de7c073-36d0-4f29-80d8-5c56d0cbf23f
 # ╟─43c3c5c8-95bd-471e-b091-ed5f794ea0e2
 # ╟─0dec2a94-2c4f-4e4a-911d-9ab6c1a3605a
 # ╟─0cc279d4-a7ab-4642-8a48-7cb6581b94c2
@@ -244,6 +253,5 @@ This custom sidebar structure enhances the navigation experience for users explo
 # ╟─4b2f8bda-a3c1-4ccd-92e8-fb19badef84f
 # ╟─6ca67d2c-7809-4026-aabb-e2e38465210d
 # ╟─69201461-d1ee-4e4d-b17e-459508748967
-# ╟─1de7c073-36d0-4f29-80d8-5c56d0cbf23f
-# ╟─7b8cc940-fbba-42ed-b5b7-30abca9feb99
 # ╟─e3b98336-7c90-4a5c-a09c-b3d6297506e3
+# ╟─039a9c77-fed4-45b5-a8cc-cec13224ddb2
